04b3dfc58f300b6e127fd571e98c722c9513395c
dim4k <kima0012@gmail.com>
2016-11-17 11:41:18 +0100

    ES6

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    var hello = new Object();
-    hello.title = 'Hello World';
-    hello.text = 'Welcome on my new website'
+    const hello = {title:'Hello World', text:'Welcome on my new website'};
     res.render('pages/index.ejs',{hello:hello});
 });
 
--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const config = require('./config/conf.json');
 
-var index = require('./routes/index');
-//var todos = require('./routes/todos');
+const index = require('./routes/index');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -22,19 +23,16 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-//app.use('/api/v1/', todos);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
+    const err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
-var server = app.listen(3000, function() {
-    var host = 'localhost';
-    var port = server.address().port;
-    console.log('App listening at http://%s:%s', host, port);
+const server = app.listen(config.app.port, function() {
+    console.log('App listening at http://%s:%s', config.app.host, config.app.port);
 });
 
 module.exports = app;
